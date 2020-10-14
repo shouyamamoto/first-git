0f574d1986816d9ed26557e69a58ae8f00304170
yamamoto syo <syo19961113@gmail.com>
2020-10-14 23:38:34 +0900

    add gulpfile_2020/10/14

--- gulpfile.js
+++ gulpfile.js
@@ -6,4 +6,17 @@ var notify = require('gulp-notify');
 
 const { src, dest, watch, series, parallel } = require('gulp');
 
+const task = () => {
+    return src('sass/*.scss', {sourcemaps: true})
+        .pipe(sassGlob())
+        .pipe(plumber())
+        .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
+        .pipe(sass({outputStyle: 'expanded'}))
+        .pipe(gulp.dest('dist/css'))
+        .pipe(gulp.dest('dist/css', {sourcemaps: true}))
+}
 
+const keiryoWatch = () => {
+    gulp.watch('sass/*.scss', task)
+}
+exports.keiryoWatch = keiryoWatch;
